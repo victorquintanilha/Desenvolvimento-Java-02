512f0cddef378e227822ae96af78413d25d859f5
Victor Juan Bernardo Quintanilha <victorjuan_7@hotmail.com>
2019-05-14 22:08:11 -0300

    Aula 14/05/2019

---
+++ lista01web/JavaSource/br/edu/web/ProgressaoBean.java
@@ -0,0 +1,62 @@
+package br.edu.web;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.edu.lista01.Exercicios;
+
+/**
+ * Calculo da progressão aritmética.
+ * 
+ * @author 1829203
+ *
+ */
+@Named
+@RequestScoped
+public class ProgressaoBean {
+	
+	private Double a1;
+	
+	private Integer n;
+	
+	private Double r;
+	
+	private Double an;
+	
+	public void calcular() {
+		this.an = Exercicios.calcularNEsimoTermo(a1, n, r);
+	}
+
+	public Double getA1() {
+		return a1;
+	}
+
+	public void setA1(Double a1) {
+		this.a1 = a1;
+	}
+
+	public Integer getN() {
+		return n;
+	}
+
+	public void setN(Integer n) {
+		this.n = n;
+	}
+
+	public Double getR() {
+		return r;
+	}
+
+	public void setR(Double r) {
+		this.r = r;
+	}
+
+	public Double getAn() {
+		return an;
+	}
+
+	public void setAn(Double an) {
+		this.an = an;
+	}
+	
+}
